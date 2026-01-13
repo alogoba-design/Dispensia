6c15dd64634d152a89c28e764415348683291aa2
alogoba-design <alogoba@gmail.com>
2026-01-12 21:40:37 -0500

    nuevo_diseño_v59

--- dispensia.js
+++ dispensia.js
@@ -148,6 +148,7 @@ function renderSteps(c){
 
 /* ===== AGREGAR A SEMANA (FIX DESKTOP) ===== */
 window.addCurrentPlate=function(){
+  event?.stopPropagation();
   if(!currentPlate) return;
 
   if(!week.some(w=>w.codigo===currentPlate.codigo)){
